250a90678d462ffd28aa13d737481bd559957b8b
Caroline Ehab <68237645+CarolineEhabAnwar@users.noreply.github.com>
2022-11-16 22:50:07 +0200

    feature/app skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import words from "./assets/wordList.json";
+import HangmanDrawing from "./containers/HangmanDrawing";
+import HangmanWord from "./containers/HangmanWord";
+import Keyboard from "./containers/Keyboard";
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words?.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
   return (
- <h1>hi</h1>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
