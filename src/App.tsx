6a1edd58c58897fc2e6ddd2a9313050899ed1a07
Caroline Ehab <68237645+CarolineEhabAnwar@users.noreply.github.com>
2022-11-16 23:52:37 +0200

    feature/pressing key event handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import words from "./assets/wordList.json";
 import HangmanDrawing from "./containers/HangmanDrawing/HangmanDrawing";
 import HangmanWord from "./containers/HangmanWord/HangmanWord";
@@ -14,6 +14,33 @@ function App() {
   const incorrectLetters = guessedLetters?.filter(
     (letter) => !wordToGuess.includes(letter)
   );
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters?.includes(letter)) {
+        return;
+      }
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e?.key;
+      if (!key?.match(/^[a-z]$/)) {
+        return;
+      }
+      e?.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", handler);
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
   return (
     <div
       style={{
