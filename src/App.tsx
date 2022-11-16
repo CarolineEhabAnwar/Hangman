192fc4b69e14a19ed595eea4b2fe182f8ad60f97
Caroline Ehab <68237645+CarolineEhabAnwar@users.noreply.github.com>
2022-11-16 23:32:15 +0200

    feature/word component logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
+  const incorrectLetters = guessedLetters?.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
   return (
     <div
       style={{
@@ -23,8 +26,8 @@ function App() {
       }}
     >
       <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
-      <HangmanDrawing />
-      <HangmanWord />
+      <HangmanDrawing numberOfGuesses={incorrectLetters?.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
       </div>
